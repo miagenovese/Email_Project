96f1cbacdd789bdf174977c5034025f9358ab29d
Mia <miagenovese@gmail.com>
2018-05-04 20:35:50 +0200

    Create email syntax.

--- Email_Project/src/email_app/Email.java
+++ Email_Project/src/email_app/Email.java
@@ -7,9 +7,11 @@ public class Email {
 	private String lastName;
 	private String password;
 	private String department;
+	private String email;
 	private int mailboxCapacity;
 	private int defaultPasswordLength = 10;
 	private String alternateEmail;
+	private String companySuffix = "company.com";
 	
 	// Constructor for receiving first and last name
 	public Email(String firstName, String lastName) {
@@ -27,6 +29,10 @@ public class Email {
 	// Call a method that returns a random password
 	this.password = randomPassword(defaultPasswordLength);
 	System.out.println("Your password is " + this.password);
+	
+	// Combine elements to generate email
+	email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
+	System.out.println("Your email is: " + email);
 	}
 	
 	// Ask for the department
